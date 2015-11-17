b5a040537ad5b13865ccff58e9721cd53f2f6572
JustWe <lfk_dsk@hotmail.com>
2015-11-17 22:57:33 +0800

    UPDATE

---
+++ app/src/main/java/com/lfk/justwetools/View/FlashTextView/FlashTextView.java
@@ -0,0 +1,70 @@
+package com.lfk.justwetools.View.FlashTextView;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by liufengkai on 15/11/6.
+ */
+public class FlashTextView extends TextView {
+    private int mViewWidth = 0;
+    private Paint mPaint;
+    private LinearGradient mLinearGradient;
+    private Matrix mGradientMatrix;
+    private int mTranslate = 0;
+
+    public FlashTextView(Context context) {
+        super(context);
+    }
+
+    public FlashTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (mViewWidth == 0) {
+            mViewWidth = getMeasuredWidth();
+            if (mViewWidth > 0) {
+                mPaint = getPaint();
+                mLinearGradient = new LinearGradient(
+                        0,
+                        0,
+                        mViewWidth,
+                        0,
+                        new int[]{
+                                Color.BLUE,
+                                Color.WHITE,
+                                Color.BLUE
+                        },
+                        null,
+                        Shader.TileMode.CLAMP);
+                mPaint.setShader(mLinearGradient);
+                mGradientMatrix = new Matrix();
+            }
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (mGradientMatrix != null) {
+            mTranslate += mViewWidth / 5;
+            if (mTranslate > 2 * mViewWidth) {
+                mTranslate = -mTranslate;
+            }
+            mGradientMatrix.setTranslate(mTranslate, 0);
+//            Log.d("translate" ,mTranslate+"");
+            mLinearGradient.setLocalMatrix(mGradientMatrix);
+            postInvalidateDelayed(100);
+        }
+    }
+}
